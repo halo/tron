41afd46add339088488e7503ea355624c7807fb2
halo <github@posteo.org>
2015-10-20 18:33:53 +0200

    Version bump 0.0.2 -> 0.1.0

--- lib/train/version.rb
+++ lib/train/version.rb
@@ -1,8 +1,8 @@
 module Train
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 0
-    TINY  = 2
+    MINOR = 1
+    TINY  = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join '.'
