47698728b378ca25f8743dd31f2b781a5913ef8c
halo <github@posteo.org>
2019-08-17 11:28:07 +0200

    Version bump 0.7.1 -> 1.0.0

--- lib/tron/version.rb
+++ lib/tron/version.rb
@@ -1,8 +1,8 @@
 module Tron
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 7
-    TINY  = 1
+    MAJOR = 1
+    MINOR = 0
+    TINY  = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join '.'
