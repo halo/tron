b37a06e705f1d33bd6e1d704ef1221d3ed6bcfaf
halo <github@posteo.org>
2019-08-17 11:23:38 +0200

    Updated rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,9 @@ require 'tron'
 require 'hashie/mash' if ENV['LOAD_HASHIE'].to_s != ''
 
 RSpec.configure do |config|
-
-  config.raise_errors_for_deprecations!
   config.disable_monkey_patching!
+  config.raise_errors_for_deprecations!
   config.color = true
+  config.order = :random if ENV['CI']
   config.fail_fast = true
-
 end
