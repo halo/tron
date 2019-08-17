b852c6a5423c780581c2c4e050288bb20f17f446
halo <github@posteo.org>
2019-08-17 11:17:36 +0200

    POC for next-generation implementation Based on an immutable Struct Inspired by https://github.com/iconara/immutable_struct Just even more minimalistic

--- lib/tron.rb
+++ lib/tron.rb
@@ -1,8 +1,53 @@
 require 'tron/version'
 
-require 'tron/resultable'
-require 'tron/success'
-require 'tron/failure'
+require 'tron/resultable' # Legady
+require 'tron/success' # Legacy
+require 'tron/failure' # Legacy
 
 module Tron
+  def self.success(code, attributes = {})
+    Struct.new(:success, *attributes.keys) do
+      undef_method '[]='.to_sym
+      members.each { |member| undef_method "#{member}=".to_sym }
+
+      def success?
+        true
+      end
+
+      def failure?
+        false
+      end
+
+      def on_success(proc = nil, &block)
+        (proc || block).call
+      end
+
+      def on_failure(_ = nil)
+        self
+      end
+    end.new code.freeze, *attributes.values.map(&:freeze)
+  end
+
+  def self.failure(code, attributes = {})
+    Struct.new(:failure, *attributes.keys) do
+      undef_method '[]='.to_sym
+      members.each { |member| undef_method "#{member}=".to_sym }
+
+      def success?
+        false
+      end
+
+      def failure?
+        true
+      end
+
+      def on_success(_ = nil)
+        self
+      end
+
+      def on_failure(proc = nil, &block)
+        (proc || block).call
+      end
+    end.new code.freeze, *attributes.values.map(&:freeze)
+  end
 end
